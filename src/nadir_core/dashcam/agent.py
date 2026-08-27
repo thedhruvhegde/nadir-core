2d4294a71f9d5bc1823ad262da912cfe23c5c5e6
Dhruv Hegde <ddvhegde100@gmail.com>
2026-08-27 13:45:19 -0400

    frame pipeline bits again

--- src/nadir_core/dashcam/agent.py
+++ src/nadir_core/dashcam/agent.py
@@ -8,3 +8,13 @@ from nadir_core.dashcam.health import HealthSample, HealthSeries
 from nadir_core.dashcam.store import JsonlStore
 from nadir_core.dashcam.types import FramePacket
 from nadir_core.dashcam.upload import upload_sample
+from nadir_core.dashcam.vision.explain import explain_mount
+from nadir_core.dashcam.vision.mount import MountTracker
+
+
+class DashcamAgent:
+    def __init__(self, config: Optional[DashcamConfig] = None) -> None:
+        self.config = config or DashcamConfig.from_env()
+        self.tracker = MountTracker(max_width=self.config.max_width)
+        self.series = HealthSeries()
+        self.store = JsonlStore(self.config.store_path)
