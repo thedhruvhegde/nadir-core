33947aba8e5bd45670225405a0f701b9d17ace04
Dhruv Hegde <ddvhegde100@gmail.com>
2026-08-28 14:44:59 -0400

    wip dashcam ingest

---
+++ tests/dashcam/test_vision.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import numpy as np
+
+from nadir_core.dashcam.sources.synthetic import make_road_frame
+from nadir_core.dashcam.vision.horizon import estimate_horizon
+from nadir_core.dashcam.vision.vanishing import estimate_vanishing_yaw
