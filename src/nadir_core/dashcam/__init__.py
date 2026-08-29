6b57a1acfbcbeae61f9f340034f745d92b35c020
Dhruv Hegde <ddvhegde100@gmail.com>
2026-08-29 10:28:51 -0400

    tests

---
+++ src/nadir_core/dashcam/__init__.py
@@ -0,0 +1,8 @@
+"""Dashcam mount / geometry health agent (Open Core)."""
+
+from nadir_core.dashcam.bridge import vision_to_readings, score_mount_estimate
+from nadir_core.dashcam.health import HealthSeries, HealthSample
+from nadir_core.dashcam.types import FramePacket, MountEstimate, SourceKind
+
+__all__ = [
+    "FramePacket",
